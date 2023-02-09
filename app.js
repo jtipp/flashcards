bdada3327444f1770ddf9f0e84bf78bca3c87c14
Karel Minarik <karel.minarik@gmail.com>
2023-02-09 18:29:54 +0100

    Add cycling throught the list of cards

--- app.js
+++ app.js
@@ -1,40 +1,61 @@
 const data = [
   ["Food", "Jídlo"],
   ["Sea", "Moře"],
+  ["Computer", "Počítač"],
 ];
+
+const title = document.getElementsByTagName("h1")[0];
+title.textContent += `(${data.length})`;
 
 const app = document.getElementById("app");
 
 const listEl = document.createElement("div");
 listEl.setAttribute("class", "cards");
 
+var currentCardIndex = 0;
+
+const nextButtonEl = document.createElement("button");
+nextButtonEl.textContent = "Next card →";
+nextButtonEl.addEventListener("click", () => {
+  currentCardIndex++;
+  if (currentCardIndex > data.length - 1) {
+    currentCardIndex = 0;
+  }
+
+  const cardEls = document.querySelectorAll(".card");
+
+  cardEls.forEach((el) => el.setAttribute("class", "card hidden"));
+
+  cardEls[currentCardIndex].setAttribute("class", "card visible");
+});
+
 data.forEach((item) => {
   const q = item[0];
   const a = item[1];
 
   const itemEl = document.createElement("div");
-  itemEl.setAttribute("class", "card");
+  itemEl.setAttribute("class", "card hidden");
   const questionEl = document.createElement("div");
   questionEl.setAttribute("class", "question");
   const answerEl = document.createElement("div");
-  answerEl.setAttribute("class", "answer");
+  answerEl.setAttribute("class", "answer hidden");
 
   const buttonEl = document.createElement("button");
   buttonEl.textContent = "Show";
   buttonEl.isHidden = true;
   buttonEl.addEventListener("click", () => {
-    console.log(buttonEl.isHidden);
     if (buttonEl.isHidden) {
       buttonEl.textContent = "Hide";
-      answerEl.textContent = "= " + a;
+      answerEl.setAttribute("class", "answer visible");
     } else {
       buttonEl.textContent = "Show";
-      answerEl.textContent = "";
+      answerEl.setAttribute("class", "answer hidden");
     }
     buttonEl.isHidden = !buttonEl.isHidden;
   });
 
   questionEl.textContent = q;
+  answerEl.textContent = "= " + a;
 
   itemEl.appendChild(buttonEl);
   itemEl.appendChild(questionEl);
@@ -44,3 +65,7 @@ data.forEach((item) => {
 });
 
 app.appendChild(listEl);
+app.appendChild(nextButtonEl);
+
+const firstCardEl = document.querySelector(".card");
+firstCardEl.setAttribute("class", "card visible");
