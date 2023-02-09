6466c9751d7f9e193432a065828b4dd28b725350
Karel Minarik <karel.minarik@gmail.com>
2023-02-09 17:34:27 +0100

    Add basic interactivity

--- app.js
+++ app.js
@@ -5,15 +5,38 @@ const data = [
 
 const app = document.getElementById("app");
 
-const list = document.createElement("ul");
+const listEl = document.createElement("dl");
 
 data.forEach((item) => {
-  const question = item[0];
-  const answer = item[1];
+  const q = item[0];
+  const a = item[1];
 
-  const listItem = document.createElement("li");
-  listItem.textContent = `${question}: ${answer}`;
-  list.appendChild(listItem);
+  const itemEl = document.createElement("div");
+  const questionEl = document.createElement("dt");
+  const answerEl = document.createElement("dd");
+
+  const buttonEl = document.createElement("button");
+  buttonEl.textContent = "Show";
+  buttonEl.isHidden = true;
+  buttonEl.addEventListener("click", () => {
+    console.log(buttonEl.isHidden);
+    if (buttonEl.isHidden) {
+      buttonEl.textContent = "Hide";
+      answerEl.textContent = a;
+    } else {
+      buttonEl.textContent = "Show";
+      answerEl.textContent = "";
+    }
+    buttonEl.isHidden = !buttonEl.isHidden;
+  });
+
+  questionEl.textContent = q;
+
+  itemEl.appendChild(buttonEl);
+  itemEl.appendChild(questionEl);
+  itemEl.appendChild(answerEl);
+
+  listEl.appendChild(itemEl);
 });
 
-app.appendChild(list);
+app.appendChild(listEl);
