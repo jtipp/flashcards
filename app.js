cec9a17ad801c2dbaeaea53761650bbdadd3a6d0
Karel Minarik <karel.minarik@gmail.com>
2023-02-09 17:48:08 +0100

    Improve visual style of the application

--- app.js
+++ app.js
@@ -5,15 +5,19 @@ const data = [
 
 const app = document.getElementById("app");
 
-const listEl = document.createElement("dl");
+const listEl = document.createElement("div");
+listEl.setAttribute("class", "cards");
 
 data.forEach((item) => {
   const q = item[0];
   const a = item[1];
 
   const itemEl = document.createElement("div");
-  const questionEl = document.createElement("dt");
-  const answerEl = document.createElement("dd");
+  itemEl.setAttribute("class", "card");
+  const questionEl = document.createElement("div");
+  questionEl.setAttribute("class", "question");
+  const answerEl = document.createElement("div");
+  answerEl.setAttribute("class", "answer");
 
   const buttonEl = document.createElement("button");
   buttonEl.textContent = "Show";
@@ -22,7 +26,7 @@ data.forEach((item) => {
     console.log(buttonEl.isHidden);
     if (buttonEl.isHidden) {
       buttonEl.textContent = "Hide";
-      answerEl.textContent = a;
+      answerEl.textContent = "= " + a;
     } else {
       buttonEl.textContent = "Show";
       answerEl.textContent = "";
