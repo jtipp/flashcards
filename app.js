977ad015f3abc50244d11aac215179445747c952
Karel Minarik <karel.minarik@gmail.com>
2023-02-11 09:00:43 +0100

    Refactor and restructure the application code (class App)
    Partically copied from ChatGPT suggestions; the prompt was: "Can you also the code so it uses an "App" class and a "run" method?"

--- app.js
+++ app.js
@@ -1,63 +1,86 @@
+class App {
+  constructor(data) {
+    this.data = data;
+    this.title = document.querySelector("h1");
+    this.app = document.getElementById("app");
+    this.listEl = document.createElement("div");
+    this.listEl.classList.add("cards");
+    this.currentCardIndex = 0;
+    this.nextButtonEl = document.createElement("button");
+    this.nextButtonEl.textContent = "Next card →";
+  }
+
+  run() {
+    this.title.textContent += `(${this.data.length})`;
+
+    this.nextButtonEl.addEventListener("click", () => this.showNextCard());
+
+    this.data.forEach((item, number) => {
+      this.createCard(item, number);
+    });
+
+    this.app.appendChild(this.listEl);
+    this.app.appendChild(this.nextButtonEl);
+
+    this.app.querySelector(".card").classList.remove("hidden");
+  }
+
+  createCard(item, number) {
+    const [q, a] = item;
+
+    const itemEl = document.createElement("div");
+    itemEl.classList.add("card", "hidden");
+
+    const cardNumberEl = document.createElement("div");
+    cardNumberEl.classList.add("number");
+    cardNumberEl.textContent = `#${number + 1}`;
+
+    const questionEl = document.createElement("div");
+    questionEl.classList.add("question");
+    const answerEl = document.createElement("div");
+    answerEl.classList.add("answer", "hidden");
+
+    const buttonEl = document.createElement("button");
+    buttonEl.textContent = "Show";
+    buttonEl.addEventListener("click", () =>
+      this.toggleCard(answerEl, buttonEl)
+    );
+
+    questionEl.textContent = q;
+    answerEl.textContent = `= ${a}`;
+
+    itemEl.appendChild(questionEl);
+    itemEl.appendChild(answerEl);
+    itemEl.appendChild(buttonEl);
+    itemEl.appendChild(cardNumberEl);
+
+    this.listEl.appendChild(itemEl);
+  }
+
+  toggleCard(answerEl, buttonEl) {
+    if (answerEl.classList.contains("hidden")) {
+      buttonEl.textContent = "Hide";
+      answerEl.classList.remove("hidden");
+    } else {
+      buttonEl.textContent = "Show";
+      answerEl.classList.add("hidden");
+    }
+  }
+
+  showNextCard() {
+    this.currentCardIndex = (this.currentCardIndex + 1) % data.length;
+
+    const cardEls = this.app.querySelectorAll(".card");
+    cardEls.forEach((el) => el.classList.add("hidden"));
+    cardEls[this.currentCardIndex].classList.remove("hidden");
+  }
+}
+
 const data = [
   ["Food", "Jídlo"],
   ["Sea", "Moře"],
   ["Computer", "Počítač"],
 ];
 
-const title = document.querySelector("h1");
-title.textContent += `(${data.length})`;
-
-const app = document.getElementById("app");
-
-const listEl = document.createElement("div");
-listEl.classList.add("cards");
-
-let currentCardIndex = 0;
-
-const nextButtonEl = document.createElement("button");
-nextButtonEl.textContent = "Next card →";
-nextButtonEl.addEventListener("click", () => {
-  currentCardIndex = (currentCardIndex + 1) % data.length;
-
-  const cardEls = document.querySelectorAll(".card");
-
-  cardEls.forEach((el) => el.classList.remove("visible"));
-  cardEls[currentCardIndex].classList.add("visible");
-});
-
-data.forEach((item, number) => {
-  const q = item[0];
-  const a = item[1];
-
-  const itemEl = document.createElement("div");
-  itemEl.classList.add("card", "hidden");
-
-  const cardNumberEl = document.createElement("div");
-  cardNumberEl.classList.add("number");
-  cardNumberEl.textContent = `#${number + 1}`;
-
-  const questionEl = document.createElement("div");
-  questionEl.classList.add("question");
-  const answerEl = document.createElement("div");
-  answerEl.classList.add("answer", "hidden");
-
-  const buttonEl = document.createElement("button");
-  buttonEl.textContent = "Show";
-  buttonEl.addEventListener("click", () => {
-    answerEl.classList.toggle("visible");
-    buttonEl.textContent = answerEl.classList.contains("visible")
-      ? "Hide"
-      : "Show";
-  });
-
-  questionEl.textContent = q;
-  answerEl.textContent = "= " + a;
-
-  itemEl.append(questionEl, answerEl, buttonEl, cardNumberEl);
-  listEl.append(itemEl);
-});
-
-app.append(listEl, nextButtonEl);
-
-const firstCardEl = document.querySelector(".card");
-firstCardEl.classList.add("visible");
+const app = new App(data);
+app.run();
